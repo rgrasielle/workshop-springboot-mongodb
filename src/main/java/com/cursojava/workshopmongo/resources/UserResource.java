0f5efa62f56df75066660ae44413f17d90d3a87d
rgrasielle <rgrasielle1@gmail.com>
2025-02-13 16:09:45 -0300

    Inserção de usuário com POST

--- src/main/java/com/cursojava/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cursojava/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.cursojava.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursojava.workshopmongo.domain.User;
 import com.cursojava.workshopmongo.dto.UserDTO;
@@ -37,5 +40,17 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));  // a resposta será o obj convertido para UserDTO
 	}
+	
+	// Método para inserir um usuário
+	
+		@RequestMapping(method=RequestMethod.POST)  // ou @PostMapping
+		public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+			User obj = service.fromDTO(objDto);  // converte o UserDTO para User
+			obj = service.insert(obj);  // insere no banco de dados
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();  // pega a url do novo recurso inserido
+			return ResponseEntity.created(uri).build();  // retorna o código HTTP 201 
+		
+		}
+	
 }
    
