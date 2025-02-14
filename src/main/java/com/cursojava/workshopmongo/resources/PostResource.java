a408a5b0715721eeb7f9a629b64fa08a88ad368a
rgrasielle <rgrasielle1@gmail.com>
2025-02-14 15:04:03 -0300

    Consulta simples com query methods

--- src/main/java/com/cursojava/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cursojava/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.cursojava.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.workshopmongo.domain.Post;
+import com.cursojava.workshopmongo.resources.util.URL;
 import com.cursojava.workshopmongo.services.PostService;
 
 @RestController
@@ -24,5 +28,14 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);  // a resposta será o obj convertido para UserDTO
 	}
-} 
+	
+	// Método para buscar o post contendo um dado string no título
+	
+		@RequestMapping(value="/titlesearch", method=RequestMethod.GET)  // caminho: /users/{id}
+		public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+			text = URL.decodeParam(text);  // decodifica o texto
+			List<Post> list = service.findByTitle(text);
+			return ResponseEntity.ok().body(list);  
+		}
+}
    
